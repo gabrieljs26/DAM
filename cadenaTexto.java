e7093ad8fc8c053d778ff886ad5aa1e8ba568c1a
gabrieljs26 <gabrieljs26.12@gmail.com>
2025-02-10 16:53:52 +0100

    Sincronizacion de archivos

---
+++ cadenaTexto.java
@@ -0,0 +1,16 @@
+public class cadenaTexto{
+
+        public static void main(String[]args){
+
+            Scanner teclado = new teclado (System.in);
+            System.out.print ("Introduzca su Edad :")
+            int Edad = teclado.nextInt();
+            System.out.println ("Su edad es : " + Edad);
+
+
+
+        }
+
+
+
+}
