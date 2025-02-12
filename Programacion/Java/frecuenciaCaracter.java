b3a1a539da750accd0fb54d29df9e1170d56e531
gabrieljs26 <gabrieljs26.12@gmail.com>
2025-02-12 12:56:48 +0100

    Orden

---
+++ Programacion/Java/frecuenciaCaracter.java
@@ -0,0 +1,27 @@
+public class frecuenciaCaracter {
+
+
+    public static void main(String[] args) {
+        
+        //Dada una cadena y un carácter, cuenta cuántas veces aparece dicho carácter en la cadena.
+        //Ejemplo de Entrada
+        //Ingrese una cadena: "programación en Java"
+        //Ingrese un carácter: 'a'
+        //Ejemplo de Salida
+        //El carácter 'a' aparece 4 veces en la cadena.
+     
+     
+        String frase = "programacion en java";
+        char[] frase2 = frase.toCharArray();
+        char caracterBuscado = 'a'; 
+        int contador = 0;
+
+        for (char c : frase2) {
+            if (c == caracterBuscado) {
+                contador++;
+            }
+        }
+
+        System.out.println("El carácter '" + caracterBuscado + "' aparece " + contador + " veces en la cadena.");
+    }
+}
